cfd6b60cb03498e9113ee57497bc5f6b8784922a
Daniel Ohrin <xqiam@outlook.com>
2022-09-13 13:43:55 -0500

    Added Carbs.js

---
+++ scripts/Carbs.js
@@ -0,0 +1,28 @@
+import { getCarbs } from "./database.js"
+
+const carbs = getCarbs()
+
+export const listCarbs = () => {
+    let html = `<ul>`
+
+    for (const carb of carbs) {
+        html += `<li>
+        <input type="radio" name="carb" value=${carb.id}" /> ${carb.name}
+        <li>`
+    }
+
+    html += `</ul>`
+
+    return html
+}
+
+// document.addEventListener(
+//     "change",
+//     e => {
+//         if (e.target.name="carb") {
+//             const carbId = e.target.value
+
+//             setCarbs(carbs.find(carb => carb.id === carbId))
+//         }
+//     }
+// )
