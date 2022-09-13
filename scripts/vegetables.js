b8894578149f8f1d278a799f80a6fccfd99cc3ea
Joshua Johns <joshuajohns28@outlook.com>
2022-09-13 13:33:40 -0500

    made vegetable module

---
+++ scripts/vegetables.js
@@ -0,0 +1,25 @@
+import { getVegtables, setVegtables } from "./database.js"
+
+const veggies = getVegtables()
+
+// document.addEventListener(
+//     "change",
+//     (event) => {
+//         if (event.target.name === "vegie") {
+//             setVegtables(parseInt(event.target.value))
+//         }
+//     }
+// )
+
+export const listVegetables = () => {
+    let html = "<ul>"
+
+    for (const veggie of veggies) {
+        html += `<li>
+        <input type="radio" name="vegie" value="${veggie.id}" /> ${veggie.name}
+        </li>`
+    }
+
+    html += "</ul>"
+    return html
+}
