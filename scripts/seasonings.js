b1a809be1abe1c354eaf5ce1a5cfc9f69b90921f
Joshua Johns <joshuajohns28@outlook.com>
2022-09-14 11:38:47 -0500

    added seasonings module

---
+++ scripts/seasonings.js
@@ -0,0 +1,25 @@
+import { getSeasonings } from "./database.js"
+
+const Seasonings = getSeasonings()
+
+// document.addEventListener(
+//     "change",
+//     (event) => {
+//         if (event.target.name === "seasoning") {
+//             setSeasonings(parseInt(event.target.value))
+//         }
+//     }
+// )
+
+export const listSeasonings = () => {
+    let html = "<ul>"
+
+    for (const seasoning of Seasonings) {
+        html += `<li>
+        <input type="radio" name="seasoning" value="${seasoning.id}" /> ${seasoning.name}
+        </li>`
+    }
+
+    html += "</ul>"
+    return html
+}
